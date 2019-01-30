a2005f1b16244d90b42efbb80b899daa8ada9b96
Audrey Balat <audrey.balat028@gmail.com>
2019-01-30 19:45:24 +0100

    Finalisation page ajout projet avec possibilité d'ajouter une pièce jointe

--- pages/formulaires/form_ajout_projet.php
+++ pages/formulaires/form_ajout_projet.php
@@ -4,13 +4,11 @@
 * d'ajouter un nouveau projet
 **/
 ?>
-<form action = "index.php?page=form_ajout_projet.php" method = "POST">
+<form enctype="multipart/form-data" action = "index.php?page=form_ajout_projet.php" method = "POST">
 
         
         <h1>Saisir un projet</h1>
         
-
-
 		</BR></BR>
 
 		Client : <?php echo $_SESSION['firstname'] . ' ' . $_SESSION['name'] ; ?></BR></BR>
@@ -29,7 +27,8 @@
         Description :</BR>
         <TEXTAREA name="description" rows=4 cols=40></TEXTAREA></BR></BR>
 
-        Fichier Joint : <input type="file" name="descriptionJoint" /></BR>
+        Fichier Joint : (Le fichier doit être nommé : nom du projet_nom du client_annee) </BR>
+        <input type="file" name="descriptionJoint" /></BR>
 
         <p>Attribution automatique :</p>
 
@@ -49,21 +48,39 @@
 
 		<input type = "submit" value = "Soumettre" name = "btn_submit"/>
 
+        </BR>
 
 </form>
 
 <?php
 
+//Si le formulaire a été envoyé
+
 if(isset($_POST['btn_submit'])) {
-    //Si le formulaire a été envoyé
 
     //Si les champs Client, Titre, et (description et/ou fichier joint) sont présent alors je peux inserer le nouveau projet dans la base
-    if ( !empty($_POST['title'] ) AND ( !empty($_POST['description']) OR !empty($_POST['descriptionJoint'])) )
+    if ( !empty($_POST['title'] ) AND ( !empty($_POST['description']) OR !empty($_FILES['descriptionJoint'])) )
     {
+        $target_path = "";
+        if ($_FILES['descriptionJoint']['size'] <> 0){
+
+            $target_path = "./Sujet_Client_pdf/";
+    
+            $target_path = $target_path . basename( $_FILES['descriptionJoint']['name']); 
+            
+            if(move_uploaded_file($_FILES['descriptionJoint']['tmp_name'], $target_path)) {
+                echo "Fichier ajouter avec succès"; 
+                echo "</BR>" ;
+            } else{
+                echo "Une erreur s'est produite lors l'enregistrement du fichier, réésayez!";
+                exit();
+            }
+        }
+
         //Permet de récupérer l'id du client
         $idCustomer = getIdPeople( $_SESSION['name']  ,  $_SESSION['firstname']);
         
-        insertNewProject($idCustomer[0], $_POST['title'], $_POST['nbStudent'], $_POST['description'], $_POST['descriptionJoint'], $_POST['automatique']);
+        insertNewProject($idCustomer[0], $_POST['title'], $_POST['nbStudent'], $_POST['description'], $target_path, $_POST['automatique']);
     }
     else 
     {
