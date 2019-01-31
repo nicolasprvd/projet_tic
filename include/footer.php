83fbdd864014ac50a36fb8c4bbe49ac633abd093
Poussver <tiff.bureau@live.fr>
2019-01-31 22:45:12 +0100

    Modifications Footer

--- include/footer.php
+++ include/footer.php
@@ -2,7 +2,7 @@
 <footer class="theFooter pas">
     <p class="footer_liens man">
         <a href="#" class="font-x-small bold upper">FAQ</a>
-        <a href="#" class="font-x-small bold upper">•</a>
+        •
         <a href="#" class="font-x-small bold upper">Contact</a>
 </footer>
 </html>
--- index.php
+++ index.php
@@ -1,57 +1,56 @@
 <?php
 
 /**
-* Index principal de l'application
-* qui inclut les pages
-**/
-  session_start();
-  require_once("include/fonctions.php");
-  require_once("include/fonctions_sql.php");
-  define('URL', 'index.php?page=');
-  
- ?>
+ * Index principal de l'application
+ * qui inclut les pages
+ **/
+session_start();
+require_once("include/fonctions.php");
+require_once("include/fonctions_sql.php");
+define('URL', 'index.php?page=');
+
+?>
 
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="keywords" content="TIC" />
-		<meta name="Projets TIC" content="Projets TIC"/>
+    <meta name="keywords" content="TIC"/>
+    <meta name="Projets TIC" content="Projets TIC"/>
     <link rel="stylesheet" href="css/style_connexion.css">
-      <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/style.css"/>
     <title>Gestion des projets TIC</title>
-  </head>
-  <body>
+</head>
 
-    <!-- ENTETE -->
-    <header>
-      <?php
-        include "include/header.php";
-        include "include/menu.php"; 
-      ?>
-    </header>
+<body class="man">
 
-    <!-- CONTENU -->
-    <div>
-      <?php
-        //Si $_GET['page'] n'est pas définie alors on affiche la page d'accueil
-        if(!isset($_GET['page'])) {
-          include "include/accueil.php";
-        }else if(isset($_GET['page']) && preg_match('/(form)/', $_GET['page'])){
-          //Si la page est définie et que c'est un formulaire alors on l'inclut
-          include "pages/formulaires/".$_GET['page'];
-        }else {
-          //Si la page est définie alors on l'inclut
-          include "pages/".$_GET['page'];
-        }
-      ?>
-    </div>
+<!-- ENTETE -->
+<header>
+    <?php
+    include "include/header.php";
+    include "include/menu.php";
+    ?>
+</header>
 
-    <!-- PIED DE PAGE -->
-    <footer>
-      <?php include "include/footer.php"; ?>
-    </footer>
+<!-- CONTENU -->
+<div>
+    <?php
+    //Si $_GET['page'] n'est pas définie alors on affiche la page d'accueil
+    if (!isset($_GET['page'])) {
+        include "include/accueil.php";
+    } else if (isset($_GET['page']) && preg_match('/(form)/', $_GET['page'])) {
+        //Si la page est définie et que c'est un formulaire alors on l'inclut
+        include "pages/formulaires/" . $_GET['page'];
+    } else {
+        //Si la page est définie alors on l'inclut
+        include "pages/" . $_GET['page'];
+    }
+    ?>
+</div>
 
-  </body>
+<!-- PIED DE PAGE -->
+<?php include "include/footer.php"; ?>
+
+</body>
 </html>
