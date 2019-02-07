a67ad1b8c861c5fd1b40551441821bd86cdad562
Audrey Balat <audrey.balat028@gmail.com>
2019-02-07 19:21:42 +0100

    Preparation page attribution projet

--- include/menu.php
+++ include/menu.php
@@ -18,6 +18,22 @@
         <ul>
           <li><a href=<?php echo URL.'liste_projets.php' ?>>Les projets</a></li>
           <li><a href=<?php echo URL.'form_ajout_projet.php'?>>Ajouter un projet</a></li>
+          <!-- Si l'admin uniquement est connectée -->
+          <?php 
+          if ($_SESSION['status'] == 1){
+          ?>
+            <li><a href=<?php echo URL.'attribution_projets_automatique.php'?>>Attributions des projets</a></li>
+          <?php 
+          }
+
+          // Si le reponsable uniquement est connecté
+          if ($_SESSION['status'] == 3){
+          ?>
+            <li><a href=<?php echo URL.'attribution_projets_manuel.php'?>>Attributions des projets</a></li>
+          <?php
+          }
+          ?>
+
         </ul>
       </nav>
       <?php
