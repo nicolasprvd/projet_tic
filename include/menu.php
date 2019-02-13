c88f35eb2164d32a8d0541360e9720339328e6b6
Poussver <tiff.bureau@live.fr>
2019-02-13 22:09:54 +0100

    Jambon

--- include/menu.php
+++ include/menu.php
@@ -1,35 +1,9 @@
-<?php
-/**
-* Menu de l'application
-**/
-?>
+<div id="logo_universite"></div>
 
-<h2>Menu</h2>
-
-<?php
-
-  //Si une connexion existe
-  if(estConnecte()) {
-
-    //Si l'admin ou un responsable projet est connecté
-    if($_SESSION['status'] == 1 || $_SESSION['status'] == 3) {
-      ?>
-      <nav>
-        <ul>
-          <li><a href=<?php echo URL.'liste_projets.php' ?>>Les projets</a></li>
-          <li><a href=<?php echo URL.'form_ajout_projet.php'?>>Ajouter un projet</a></li>
-        </ul>
-      </nav>
-      <?php
-    }else if($_SESSION['status'] == 2) {
-      ?>
-      <nav>
-        <ul>
-          <li><a href=<?php echo URL.'liste_projets.php' ?>>Les projets</a></li>
-        </ul>
-      </nav>
-      <?php
-    }
-  }
-
- ?>
+<nav id="nav" role="navigation">
+    <ul id="main-menu">
+        <li>
+            <a href="../index.php">Accueil</a>
+        </li>
+    </ul>
+</nav>
