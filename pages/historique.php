2686ea7d293ef1a7b69fdf955243e5ed36249794
Audrey Balat <audrey.balat028@gmail.com>
2019-03-10 19:29:44 +0100

    Preparation page historique

--- pages/historique.php
+++ pages/historique.php
@@ -5,3 +5,7 @@
 ?>
 
 <h1> Historique </h1>
+
+<img src = "./images/attention.png"   height="100px" 
+    width="200px" >
+<p> Attention, historiser vos tables uniquement lorsque vous changez d'année scolaire !!</p>
