580e2b127d224cb5f845d033004fb145050dbe72
Audrey Balat <audrey.balat028@gmail.com>
2019-03-11 22:05:29 +0100

    Preparation de la page historique

--- pages/historique.php
+++ pages/historique.php
@@ -4,8 +4,30 @@
 **/
 ?>
 
+<?php
+$verif = false;
+if(isset($_POST['btn_submit'])){
+    $verif = true;
+}
+
+?>
+
 <h1> Historique </h1>
 
 <img src = "./images/attention.png"   height="100px" 
     width="200px" >
-<p> Attention, historiser vos tables uniquement lorsque vous changez d'année scolaire !!</p>
+<p> Attention, historier vos tables uniquement lorsque vous changez d'année scolaire !!</p>
+
+<?php 
+if ($verif == false){
+    ?>
+<form enctype="multipart/form-data" action = "index.php?page=historique.php" method = "POST">
+    <input type = "submit" value = "Historier l'année" name = "btn_submit"/>
+</form>
+
+<?php
+} else {
+    echo 'On doit historier';
+}
+?>
+
