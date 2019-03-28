870c48c956e215479a11a50d6b30d7e82f58e1ee
Poussver <tiff.bureau@live.fr>
2019-03-28 17:47:52 +0100

    Modification accueil

--- include/accueil.php
+++ include/accueil.php
@@ -4,31 +4,31 @@
 
         <p>Ce site vous permettra, selon votre profil :</p>
 
-        <p class="mbn">- Si vous êtes l’administrateur de l'application :
-        <ul class="mtn">
-            <li>Attribuer les projets automatiquement</li>
-            <li>Récupérer le csv de toutes les attributions des projets/groupes</li>
-            <li>Récupérer le csv des notes de tous les étudiants</li>
-            <li>Historier la base d’une année sur l’autre</li>
-        </ul>
-        </p>
-
         <p class="mbn">- Si vous êtes un client :
         <ul class="mtn">
             <li>Ajouter un ou plusieurs nouveau(x) projet(s)</li>
-            <li>Visualiser mon/mes projets proposés</li>
-            <li>Attribuer manuellement un projet (ou attendre son attribution automatique)</li>
-            <li>Visualiser le groupe attribué à votre projet ainsi que les documents déposés par le groupe en question
-            (Cahier des charges, Gantt, rendu final)</li>
-            <li>Attribuer les notes au groupe.</li>
+            <li>Attribuer manuellement un projet</li>
+            <li>Visualiser le ou les projets proposés</li>
+            <li>Visualiser les informations sur le groupe attribué au projet, ainsi que les documents déposés par ce dernier
+                (Cahier des charges, Gantt, Rendu final)</li>
+            <li>Noter les groupes</li>
+        </ul>
+        </p>
+
+        <p class="mbn">- Si vous êtes l’administrateur de l'application :
+        <ul class="mtn">
+            <li>Avoir accès aux mêmes fonctionnalités que les clients</li>
+            <li>Attribuer automatiquement les projets</li>
+            <li>Récupérer le csv de toutes les attributions des projets/groupes, ainsi que les notes</li>
+            <li>Historier la base d’une année sur l’autre</li>
         </ul>
         </p>
 
         <p class="mbn">- Si vous êtes un étudiant :
         <ul class="mtn">
             <li>Visualiser les projets proposés par les différents clients</li>
-            <li>Choisir un ou plusieurs projet (et renseigner les membres du groupe)</li>
-            <li>Visualiser son projet une fois celui ci attribué ainsi que déposer les documents pour le client (Cahier des
+            <li>Se positioner sur un ou plusieurs projet (et renseigner les membres du groupe)</li>
+            <li>Visualiser son projet une fois ce dernier attribué, ainsi que déposer les documents demandés (Cahier des
                 charges, Gantt, rendu final)</li>
         </ul>
         </p>
